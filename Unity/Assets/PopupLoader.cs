4400372b8f652144d322181e654a389937637061
daydreaam <dayingyeo@naver.com>
2026-08-03 23:38:15 +0900

    1. 메인화면 버그 수정 + 재테스트

--- Unity/Assets/PopupLoader.cs
+++ Unity/Assets/PopupLoader.cs
@@ -2,13 +2,24 @@ using UnityEngine;
 
 public class PopupLoader : MonoBehaviour
 {
+    [SerializeField]
+    private Transform popupFolder;
     public void OpenPopup(GameObject gameObject)
     {
+        CloseAllPopup();
         gameObject.SetActive(true);
     }
 
     public void ClosePopup(GameObject gameObject)
     {
         gameObject.SetActive(false);
+    }
+
+    public void CloseAllPopup()
+    {
+        foreach(Transform child in popupFolder)
+        {
+            child.gameObject.SetActive(false);
+        }
     }
 }
